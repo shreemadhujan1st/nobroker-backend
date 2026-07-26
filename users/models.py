a24c95f785353453644b2ee36bc207220c11b6cf
Shree Madhu <shreemadhu123@gmail.com>
2026-07-26 17:25:33 +0530

    Completed user registration fixes

--- users/models.py
+++ users/models.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+
     phone = models.CharField(
         max_length=15,
         unique=True,
         blank=True,
         null=True,
+    )
+
+    is_premium = models.BooleanField(
+        default=False,
     )
 
     def __str__(self):
